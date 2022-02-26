d14cfbd0db3cd4f182fcf119ae799259d2555a81
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-02-27 05:16:32 +0800

    Modified script.js - Get password string

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 // prompt pwd length min 8, max 128 characters. 
 function getInput() {
     let charLength = prompt("Please enter the character length from 8 to 128: ");
@@ -22,53 +20,73 @@ function checkInput() {
     }
 }
 
-function charType() {
-    // prompt lowercase? uppercase? numeric? and/or special characters?
-    let lwCase = confirm("Do you want to generate lowercase?");
-    let upCase = confirm("Do you want to generate uppercase?");
-    let numeric = confirm("Do you want to generate numeric?");
-    let spChar = confirm("Do you want to generate special character?");
-    
-    let charTypes = [];
+function randomItem(items) {
+    return items[Math.floor(Math.random()*items.length)];
+}
 
-    if (lwCase) {
+function pwdString(charLength) {
+    let charTypes = [];
+    console.log("the char length is : " + charLength);
+    let lwCaseGen = confirm("Do you want to generate lowercase?");
+    let upCaseGen = confirm("Do you want to generate uppercase?");
+    let numGen = confirm("Do you want to generate numeric?");
+    let spCharGen = confirm("Do you want to generate special character?");
+    if (lwCaseGen) {
         charTypes.push("lw");
     }
 
-    if(upCase) {
+    if(upCaseGen) {
         charTypes.push("up");
     }
 
-    if(numeric) {
+    if(numGen) {
         charTypes.push("num");
     }
 
-    if(spChar) {
+    if(spCharGen) {
         charTypes.push("sp");
     }
 
     // check for at least one character type is selected
     if(!charTypes.length) {
         window.alert("You need to choose at least one character type. Please select again");
-        charType();
+        pwdString(charLength);
     }else {
-        console.log("return random characters: " + charTypes);
-        return charTypes;
+        //console.log("return random characters: " + charTypes);
+        console.log("The char types are: " + charTypes);
+
+        let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+        let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+        let numbers = ["1","2","3","4","5","6","7","8","9","0"];
+        let spChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "/", "?"];
+        
+        let pwdString = "";
+    
+        for(i=0; i<charLength; i++) {
+            let randomType = randomItem(charTypes);
+            console.log("random type: " + randomType);
+    
+            if(randomType == "lw") {
+                pwdString = pwdString + randomItem(lowerCase);
+                console.log("give me a random lower case character: " + pwdString);
+            }
+            else if(randomType == "up") {
+                pwdString = pwdString + randomItem(upperCase)
+                console.log("give me a random upper case character: " + pwdString);
+            } 
+            else if(randomType == "num") {
+                pwdString = pwdString + randomItem(numbers)
+                console.log("give me a random number: " + pwdString);
+            }
+            else {
+                pwdString = pwdString + randomItem(spChars)
+                console.log("give me a random special character: " + pwdString);
+            }   
+        }
     }
 }
 
-
-
-// generate password
-
-// display password
-
 function genPwd() {
-    let alphabets =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    let numbers = [1,2,3,4,5,6,7,8,9,0];
-    let spChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "/", "?"]
-
-    checkInput();
-    charType();
-
+    let charLength = checkInput();
+    pwdString(charLength);
 }
